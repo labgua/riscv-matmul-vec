deea4968ca9e2ec5ace7a239619c10c2c19eb3d4
Sergio Guastaferro <sergio.guastaferro@yahoo.it>
2026-02-24 11:07:45 +0100

    fix config params

--- benchmark.sh
+++ benchmark.sh
@@ -7,19 +7,19 @@ PREFIX="bench$(date +%d%m%y)"
 # baseline
 echo "> baseline"
 mkdir report/baseline
-./benchsuite/benchsuite-size.sh report/baseline/$PREFIX-baseline.txt ./build/risc64/baseline
+./benchsuite/benchsuite-size.sh report/baseline/$PREFIX-baseline.txt ./build/riscv64/baseline
 
 # autovect
 echo "> autovect"
 mkdir report/autovect
-./benchsuite/benchsuite-size.sh report/autovect/$PREFIX-autovect.txt ./build/risc64/autovect
+./benchsuite/benchsuite-size.sh report/autovect/$PREFIX-autovect.txt ./build/riscv64/autovect
 
 # tiling_v1
 echo "> tiling_v1"
 mkdir report/tiling_v1
 ./benchsuite/benchsuite-params.sh \
     report/tiling_v1/$PREFIX-tiling_v1.txt \
-    ./build/risc64/tiling \
+    ./build/riscv64/tiling \
     SIZE="256,512,1024,2048,4096" \
     KERNEL="2,4,8" \
     LMUL="1"
@@ -29,7 +29,7 @@ echo "> tiling_v2"
 mkdir report/tiling_v2
 ./benchsuite/benchsuite-params.sh \
     report/tiling_v2/$PREFIX-tiling_v2.txt \
-    ./build/risc64/tiling_v2 \
+    ./build/riscv64/tiling_v2 \
     SIZE="256,512,1024,2048,4096" \
     KERNEL="2,4,8,16" \
     LMUL="1"
@@ -39,7 +39,7 @@ echo "> tiling_v3"
 mkdir report/tiling_v3
 ./benchsuite/benchsuite-params.sh \
     report/tiling_v3/$PREFIX-tiling_v3.txt \
-    ./build/risc64/tiling_v3 \
+    ./build/riscv64/tiling_v3 \
     SIZE="256,512,1024,2048,4096" \
     KERNEL="2,4,8,16" \
     LMUL="-2,1,2,4,8"
@@ -49,7 +49,7 @@ echo "> reordered_tiling"
 mkdir report/reordered_tiling
 ./benchsuite/benchsuite-params.sh \
     report/reordered_tiling/$PREFIX-reordered_tiling.txt \
-    ./build/risc64/reordered_tiling \
+    ./build/riscv64/reordered_tiling \
     SIZE="256,512,1024,2048,4096" \
     KERNEL="2,4,8,16" \
     LMUL="-2,1,2,4,8"
